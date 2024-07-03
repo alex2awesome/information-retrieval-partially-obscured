78534251c5cd8e00075d809b021c6a9593b350a2
Liheng Lai <liheng@berkeley.edu>
2024-07-03 12:29:51 +0800

    ads

--- prompting/prompting.sh
+++ prompting/prompting.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH --time=40:00:00
-#SBATCH --gres=gpu:4
-#SBATCH --cpus-per-gpu=10
-#SBATCH --mem=100G
-#SBATCH --partition=isi
+#SBATCH --time=1:00:00
+#SBATCH --gres=gpu:a100:4
+#SBATCH --mem-per-gpu=10GB
+#SBATCH --cpus-per-gpu=20
+#SBATCH --partition=gpu
 
 module load python/3.11
 pip install -r requirements.txt
