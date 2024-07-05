6bf6c886d919bffa8e65e6696ca6edfe43678536
Liheng Lai <liheng@berkeley.edu>
2024-07-05 21:56:39 +0800

    asdf

--- prompting/prompting.py
+++ prompting/prompting.py
@@ -1,6 +1,6 @@
 
 from vllm import LLM,  SamplingParams
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AutoModelForCausalLM
 import os
 import pdb
 
@@ -46,7 +46,7 @@ def infer(model, messages):
     return output[0].outputs[0].text
 
 
-if __name__ == "__main__":
+def main():
     
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", type=str, default="meta-llama/Meta-Llama-3-70B-Instruct")
@@ -56,6 +56,40 @@ if __name__ == "__main__":
     args = parser.parse_args()
     pdb.set_trace()
 
+
+    sources = '''The source tripulantes refers to flight attendants. They provide information about their concerns and practices during the pandemic, such as leaving food trays on an intermediate seat between them and the passenger, and wanting concrete measures to avoid contagion.'''
+
+    system_prefix = '''
+    For each given text, obscure the specific details by leaving out all important information except for a short, generalized biographical contextless description about who/what the source is.
+
+    Format:
+    INPUT: Identity information + Biographical information + Given information
+    OUTPUT: Identity information + Biographical information
+
+    Example:
+    INPUT: The Biden Administration provided information on its efforts to improve access to mental health resources in schools, including a nearly $300 million allotment to expand access to mental health care.
+    OUTPUT: The Biden Administration is the executive branch of the U.S. federal government under the leadership of President Joe Biden.
+
+    If there is no information in the original entry on what the source is, only include the source name and nothing else. However, if you are fully certain that you can infer what the source is without hallucinating, include that.
+    Execute this task on all entries below. Only include one output line for each entry, include nothing except what comes after "OUTPUT" (excluding the word"OUTPUT"):
+
+    It's important to return the obscured text only.
+    Here's the text:
+    {source}
+    '''
+
+
+
+    input_text = system_prefix.format(source=sources)
+
+    tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct")
+    model = AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct")
+    inputs = tokenizer(input_text, return_tensors="pt")
+    outputs = model.generate(**inputs, max_new_tokens=50)
+    generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+    print(generated_text)
+    return
 
 
 
@@ -120,3 +154,6 @@ if __name__ == "__main__":
 
     
     print("DONE!!!!!!!!!!!!")
+
+if __name__ == "__main__":
+    main()
