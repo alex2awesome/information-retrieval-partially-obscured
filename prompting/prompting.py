961aed5e033e02c5d93fb9ffea20a5b15f12b03a
Liheng Lai <liheng@berkeley.edu>
2024-07-07 20:39:34 +0800

    asdf

--- prompting/prompting.py
+++ prompting/prompting.py
@@ -25,6 +25,25 @@ HF_HOME = "/project/jonmay_231/spangher/huggingface_cache"
 os.environ['HF_TOKEN'] = "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"
 os.environ['HF_HOME'] = HF_HOME
 
+system_prefix = '''
+    For each given text, obscure the specific details by leaving out all important information except for a short, generalized biographical contextless description about who/what the source is.
+
+    Format:
+    INPUT: Identity information + Biographical information + Given information
+    OUTPUT: Identity information + Biographical information
+
+    Example:
+    INPUT: The Biden Administration provided information on its efforts to improve access to mental health resources in schools, including a nearly $300 million allotment to expand access to mental health care.
+    OUTPUT: The Biden Administration is the executive branch of the U.S. federal government under the leadership of President Joe Biden.
+
+    If there is no information in the original entry on what the source is, only include the source name and nothing else. However, if you are fully certain that you can infer what the source is without hallucinating, include that.
+    Execute this task on all entries below. Only include one output line for each entry, include nothing except what comes after "OUTPUT" (excluding the word"OUTPUT"):
+
+    It's important to return the obscured text only.
+    Here's the text:
+    {source}
+    '''
+
 def load_model(model):
     torch.cuda.memory_summary(device=None, abbreviated=False)
     model = LLM(
@@ -46,77 +65,60 @@ def infer(model, messages):
     return output[0].outputs[0].text
 
 
-def main():
-    
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--model", type=str, default="meta-llama/Meta-Llama-3-70B-Instruct")
-    parser.add_argument('--data_dir', type=str, default='./data')
-    # parser.add_argument('--start_idx', type=int, default=None)
-    # parser.add_argument('--end_idx', type=int, default=None)
-    args = parser.parse_args()
+def main(args):
 
+    source_file = args.source_file
+    sources_path = '../data/' + source_file + '.json'
+    with open(sources_path, 'r') as f:
+        contents = f.read()
 
-    sources_path = 'sources_data_70b__200000_200100.txt'
-    # with open(sources_path, 'r') as f:
-    #     sources = f.read()
-    #     print('sources:', sources)
-
-    sources = '''The source tripulantes refers to flight attendants. They provide information about their concerns and practices during the pandemic, such as leaving food trays on an intermediate seat between them and the passenger, and wanting concrete measures to avoid contagion.'''
-
-    system_prefix = '''
-    For each given text, obscure the specific details by leaving out all important information except for a short, generalized biographical contextless description about who/what the source is.
-
-    Format:
-    INPUT: Identity information + Biographical information + Given information
-    OUTPUT: Identity information + Biographical information
-
-    Example:
-    INPUT: The Biden Administration provided information on its efforts to improve access to mental health resources in schools, including a nearly $300 million allotment to expand access to mental health care.
-    OUTPUT: The Biden Administration is the executive branch of the U.S. federal government under the leadership of President Joe Biden.
-
-    If there is no information in the original entry on what the source is, only include the source name and nothing else. However, if you are fully certain that you can infer what the source is without hallucinating, include that.
-    Execute this task on all entries below. Only include one output line for each entry, include nothing except what comes after "OUTPUT" (excluding the word"OUTPUT"):
-
-    It's important to return the obscured text only.
-    Here's the text:
-    {source}
-    '''
-
-
-
-    prompt = system_prefix.format(source=sources)
-    # print('prompt:\n', prompt)
-    message = [
-            {
-                "role": "system",
-                "content": "You are an experienced journalist.",
-            },
-
-            {
-                "role": "user",
-                "content": prompt
-            },
-        ]
-
-    tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct")
-    formatted_prompt = tokenizer.apply_chat_template(message, tokenize=False, add_generation_prompt=True)
-
+    tokenizer = AutoTokenizer.from_pretrained(args.model)
     model = load_model(args.model)
     sampling_params = SamplingParams(temperature=0.1, max_tokens=1024)
-    outputs = model.generate([formatted_prompt], sampling_params)
-    fname = 'sources_data_70b__200000_200100_obscured.txt'
-    with open(fname, 'w') as f:
-        f.write("wtf?????")
 
-    for output in outputs:
-        content = output.outputs[0].text
-        fname = 'sources_data_70b__200000_200100_obscured.txt'
-        with open(fname, 'w') as f:
-            f.write("wtf?????")
-            f.write(content)
+    messages = []
+    urls = []
+    sources = []
 
+    for content in contents:
+        url = content['article_url']
+        source = content['source']
+        prompt = system_prefix.format(source=source)
+
+        
+        message = [
+                {
+                    "role": "system",
+                    "content": "You are an experienced journalist.",
+                },
+
+                {
+                    "role": "user",
+                    "content": prompt
+                },
+            ]
+        formatted_prompt = tokenizer.apply_chat_template(message, tokenize=False, add_generation_prompt=True)
+        messages.append(formatted_prompt)
+        urls.append(url)
+        sources.append(source)
+        
+    outputs = model.generate(messages, sampling_params)  
+
+    output_content = []
+    for url, source, output in zip(urls, sources, outputs):
+        obsc_source = output.outputs[0].text
+        output_content.append({
+            "article_url": url,
+            "source": source,
+            "obscured_source": obsc_source
+        })
     
     print("DONE!!!!!!!!!!!!")
 
+
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--source_file', type=str)
+    parser.add_argument("--model", type=str, default="meta-llama/Meta-Llama-3-70B-Instruct")
+    args = parser.parse_args()
+    main(args)
