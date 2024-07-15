fa31733ac435df0655f13e85dd397b58cca8d527
Liheng Lai <liheng@berkeley.edu>
2024-07-15 11:59:15 +0800

    ASDf

--- preprocessing/parse.sh
+++ preprocessing/parse.sh
@@ -3,9 +3,9 @@
 module load python/3.11
 pip install -r requirements.txt
 
-start_idx=$1
-step=$2
-iterations=$3
+start_idx=0
+step=100
+iterations=2729
 end_idx=$((start_idx + step))
 
 for ((i=0; i<iterations; i++)); do
