7277067073778c29f841ac1f30d7f329c773ed25
Liheng Lai <liheng@berkeley.edu>
2024-07-29 23:07:44 +0800

    asdf

--- baselines/vanilla_search.sh
+++ baselines/vanilla_search.sh
@@ -2,7 +2,8 @@
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH --time=40:00:00
-#SBATCH --gres=gpu:a40:1
+#SBATCH --gres=gpu:a100:1
+#SBATCH --constraint=a100-80gb
 #SBATCH --mem=200GB
 #SBATCH --cpus-per-gpu=10
 #SBATCH --partition=gpu
