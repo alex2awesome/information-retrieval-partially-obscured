ad423b922ef3c68b5c322d5d5f9b7121615bc3ab
Liheng Lai <liheng@berkeley.edu>
2024-08-11 20:32:24 +0800

    adsf

--- baselines/eval.py
+++ baselines/eval.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import statistics
 
 def precision(tp, fp):
     return tp / (tp + fp) 
@@ -53,6 +54,47 @@ def dummy(data):
         total += 1
     print("lalala", count/total)
 
+def get_scores(path: str):
+    precision_list = []
+    recall_list = []
+    f1_list = []
+
+    with open(path, 'r') as file:
+        contents = json.load(file)
+        for content in contents:
+            y_pred = set()
+            y_true = set()
+
+            y_true.add(content['ground_truth_source'])
+            for k in content['topk']:
+                y_pred.add(k['text'])
+            
+            if len(y_true) == 0:
+                continue
+            
+            true_pos = set.intersection(y_pred, y_true)
+            n = len(true_pos)
+
+            recall = n / len(y_true)
+            precision = n / len(y_pred)
+
+            if (recall + precision) == 0:
+                f1 = 0
+
+            else:
+                f1 = (2 * precision * recall) / (precision + recall)
+            
+            recall_list.append(recall)
+            precision_list.append(precision)
+            f1_list.append(f1)
+    
+    avg_prec = statistics.mean(precision_list)
+    avg_rec = statistics.mean(recall_list)
+    avg_f1 = statistics.mean(f1_list)
+
+    print("average precision:", avg_prec)
+    print("average recall:", avg_rec)
+    print("average f1:", avg_f1)
 
 
 
@@ -62,10 +104,12 @@ def main(args):
         search_results = json.load(f)
     precision_value, recall_value, f1_value = calculate_metrics(search_results)
 
-    print("precision:", precision_value)
-    print("recall:", recall_value)
-    print("f1:", f1_value)
-    dummy(search_results)
+    # print("precision:", precision_value)
+    # print("recall:", recall_value)
+    # print("f1:", f1_value)
+    get_scores("../data_baseline/vanilla.json")
+
+    # dummy(search_results)
 
 
 if __name__ == "__main__":
